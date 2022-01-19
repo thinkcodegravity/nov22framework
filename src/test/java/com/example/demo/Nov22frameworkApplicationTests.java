ea1f6c18165cf9df5073c1d93dc663c5c8131bc5
java <74448030+thinkcodegravity@users.noreply.github.com>
2022-01-19 12:03:01 -0600

    framwork code

---
+++ src/test/java/com/example/demo/Nov22frameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nov22frameworkApplicationTests {
+
+	@org.junit.Test
+	void contextLoads() {
+	}
+
+}
